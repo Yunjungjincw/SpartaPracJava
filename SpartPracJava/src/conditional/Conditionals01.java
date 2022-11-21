8db15592d448a889820ae8912e4372863937e856
farmer <farmer@DESKTOP-U63O9IM>
2022-11-21 20:26:39 +0900

    문법연습 22_11_21 20:27

---
+++ SpartPracJava/src/conditional/Conditionals01.java
@@ -0,0 +1,44 @@
+package conditional;
+
+public class Conditionals01 {
+
+	public static void main(String[] args) {
+
+		
+		char score = 'C';
+		switch(score) {
+		case'A':
+			System.out.println("A등급 축하합니다");
+			break;
+		case'B':
+			System.out.println("B등급입니다");
+			break;
+		case'C':
+			System.out.println("C등급입니다");
+			break;
+		default:
+			System.out.println("C보다 아래 등급입니다");
+		}
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		//		int check  = 100;
+//		int num1 = 50;
+//		
+//		if(num1 > check ) {
+//			System.out.println("100보다 큰수입니다");
+//		}else if(num1>50 ){
+//			System.out.println("50보다 큰수입니다");
+//		} else {
+//		System.out.println("50보다 작아요");
+//	}
+	}
+}
---
+++ SpartPracJava/src/dataType/Operator.java
@@ -0,0 +1,65 @@
+package dataType;
+
+public class Operator {
+
+	public static void main(String[] args) {
+		boolean a = true;
+		boolean b = false;
+		
+		//논리 연산자
+		System.out.println(a && b); // false
+		// 둘다 참이여야 한다.
+		System.out.println(a && true); // true
+		System.out.println(a || false);
+		System.out.println(b || false);
+		// 둘 중에 하나라도 참이면 참이야.
+		System.out.println(!a);
+		System.out.println(!b);
+		
+		
+		//비트연산자 - 시스템프로그래밍을 하지 않는 이상 하지 않음.
+		
+		
+		
+		// 산술연산자
+//		System.out.println(num1+num2);
+//		System.out.println(num1-num2);
+//		System.out.println(num1*num2);
+//		System.out.println(num1/num2);
+//		System.out.println(num1%num2);
+//		
+		
+		// 대입 연산자
+		// 계산한 값을 대입하겠다.
+//		System.out.println(num1);
+//		num1+=num2;
+//		System.out.println(num1);// 15
+//		
+//		num1-=num2;
+//		System.out.println(num1);//10
+//		
+//		num1 *=num2;
+//		System.out.println(num1); // 50
+//		
+//		num1/=num2;
+//		System.out.println(num1); //10
+//		
+//		num1%=num2;
+//		System.out.println(num1); // 0 
+//		System.out.println("------");
+//		
+		
+		//관계연산자
+//		System.out.println(num1>num2);
+//		System.out.println(num1>=num3);
+//		System.out.println(num1<num2);
+//		System.out.println(num1<=num3);
+//		System.out.println(num1==num3);
+//		System.out.println(num1 != num2);
+		
+		
+		
+	
+	}
+
+}
