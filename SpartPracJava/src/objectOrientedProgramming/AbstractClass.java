d59bbad802a64e470577e2ee177cebe653dcced9
Yunjungjincw <farmer@DESKTOP-U63O9IM>
2022-11-22 23:07:49 +0900

    자바문법 22_11_22 23:07

--- SpartPracJava/src/objectOrientedProgramming/AbstractClass.java
+++ SpartPracJava/src/objectOrientedProgramming/AbstractClass.java
@@ -1,8 +1,35 @@
 package objectOrientedProgramming;
 
-public class AbstractClass {
+abstract class Bird {
+	private int x,y,z;
+	
+	void fly(int x, int y, int z) {
+		printLocation();
+		System.out.println("이동합니다");
+		this.x = x;
+		this.y = y;
+		if(flyable(z)) {
+			this.z = z;
+		}else {
+			System.out.println("그 높이로는 날 수 없습니다");
+		}
+		this.z = z;
+		printLocation();
+	}
+	
+	abstract boolean flyable(int z);
+		
+	
+	public void printLocation() {
+		System.out.println("현재위치 {" +x +","+y+","+z+")");
+	}
+}
 
+
+public class AbstractClass {
 	public static void main(String[] args) {
+//		Bird bird = new Bird();
+		//Cannot instantiate the type Bird
 		
 	}
 
